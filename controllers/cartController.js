35c59011f07741262806f742a58ed04aab621d52
sangukpark <pso0301@gmail.com>
2022-11-23 13:05:10 +0900

    [Edit]장바구니 기능추가- 추가수정(error.js 추가 구현)

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,77 +1,68 @@
 const { cartService } = require('../services')
+const { catchAsync } = require("../utils/error")
+const {raiseCustomError}= require("../utils/error")
 
-const addCart = async(req,res)=>{
-    try{
-        const {userId, productOptionId, quantity}= req.body
-        console.log(userId)
-        console.log(productOptionId)
-        console.log(quantity)
-        if(!userId || !productOptionId|| !quantity){
-            return res.status(400).json({message: "Invaild Key Error"})
-        }
-        const result = await cartService.addCart(userId, productOptionId, quantity)
-        return res.status(201).json({message: "Success Input Key_Values"})
-    }catch (error) {
+const addCart = catchAsync(async(req,res)=>{
+    const {userId, productOptionId, quantity}= req.body
 
-        res.status(error.statusCode || 500).json({ message: error.message})
+    const result = await cartService.addCart(userId, productOptionId, quantity)
+    return res.status(201).json({message: "Success Input Key_Values"})
+
+});
+
+const getUserCart = catchAsync(async (req, res) => {
+    const {userId}=req.params
+
+    if(!userId) 
+    {
+        const error = new Error("userId is not define")
+        error.statusCode=400
+        throw error;
     }
-};
+       
+    const getUserCart = await cartService.getUserCart(userId);
+    res.status(200).json({data:getUserCart});
 
-const getUserCart = async (req, res) => {
-    try{
-        const {userId}=req.params
-        console.log(userId)
-        if(!userId) {
-            return res.status(400).json({message : "Wrong_userId_error"})
-        }
-            console.log(req.body)    
-            const getUserCart = await cartService.getUserCart(userId);
-            res.status(200).json({data:getUserCart});
-    }catch (error) {
-        res.status(error.statusCode || 500).json({ message: error.message})
+});
+
+const changeCartQuantity = catchAsync(async (req, res) => {
+    const { userId, productOptionId, quantity } = req.body;
+        
+    if(!userId || !productOptionId|| !quantity)
+    {
+        const error = new Error("Key Error");
+        error.statusCode=400;
+        throw error;
     }
-};
 
-const changeCartQuantity = async (req, res) => {
-
-    try{
-        const { userId, productOptionId, quantity } = req.body;
-        if(!userId || !productOptionId|| !quantity){
-            const error = new Error("Key Error");
-        }
-        console.log(req.body)
-        const changeCartQuantity = await cartService.changeCartQuantity(userId, productOptionId, quantity)
-        res.status(200).json({changeCartQuantity});
-}catch(error){
-    res.status(error.statusCode||500).json({message: error.message})
-}}
+    const changeCartQuantity = await cartService.changeCartQuantity(userId, productOptionId, quantity)
+    res.status(200).json({changeCartQuantity});
+})
     
-
-const deleteALLCart = async (req, res) => {
-    try{
-        const{userId}=req.body;
-        if(!userId) {
-            const error = new Error("ALL DELETE ERROR")
-        }
-        console.log(req.body)
-        const allDeleteCart = await cartService.allDeleteCart(userId)
-        res.status(200).json({allDeleteCart})
-    }catch(error){
-        res.status(error.statusCode||500).json({message: error.message})
+const deleteALLCart = catchAsync(async (req, res) => {
+        const{userId}=req.body
+    if(!userId) 
+    {
+        const error = new Error("ALL DELETE ERROR")
+        error.statusCode=400;
+        throw error;
     }
-    }
+    const allDeleteCart = await cartService.allDeleteCart(userId)
+    res.status(200).json({allDeleteCart})
+})
 
-const deleteCart = async (req, res) => {
-    try{const { userId, productOptionId } = req.body;
-    if(!userId || !productOptionId) {
+const deleteCart = catchAsync(async (req, res) => {
+    const { userId, productOptionId } = req.body;
+
+    if(!userId || !productOptionId) 
+    {
         const error = new Error("Something Invalid..")
+        error.statusCode=400;
+        throw error;
     }
-        console.log(req.body)
-        const deleteCart= await cartService.deleteCart(userId, productOptionId)
-        restart.status(200).json({deleteCart})
-    }catch(error){
-        res.status(error.statusCode||500).json({message:error.message})
-    }};
+    const deleteCart= await cartService.deleteCart(userId, productOptionId)
+    restart.status(200).json({deleteCart})
+});
     
 module.exports = {
     addCart,
